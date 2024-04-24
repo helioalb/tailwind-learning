7d7e51f232183354d6f89493524c46faa099c5c0
Helio Albano de Oliveira <helioalb@gmail.com>
2024-04-24 09:34:37 -0300

    Add animation example

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     extend: {
       colors: {
         primary: '#00FF00'
+      },
+      keyframes: {
+        floatUp: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-8px)' },
+          '0%': { transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        floatUp: 'floatUp 2s infinite'
       }
     },
   },
